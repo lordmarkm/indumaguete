fcea9c1608a5f5ad098a227967ef9636493e93cc
mbmartinez <mbmartinez@localhost>
2012-07-26 15:48:36 +0800

    initial commit

---
+++ indumaguete/src/main/java/indumaguete/indumaguete/App.java
@@ -0,0 +1,13 @@
+package indumaguete.indumaguete;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
